978ecdc67cf7148c006a12791287e3914d497117
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-12-09 10:56:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ let arr1=[1,2,3,-9];
 
 function mapToNegativize(arr1) {
   //arr1.map( (e) => {
+  
     for (var i = 0; i < arr1.length; i++) {
-      arr1[i] *= (-1);
+      arr1[i] = arr1[i]*(-1);
     }
   //} )
 }
